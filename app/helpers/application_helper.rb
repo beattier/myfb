35dc1ef069dbb5a2f133d9b72b129f7f2146c2ed
Todor Dragnev <todor.dragnev@gmail.com>
2011-04-27 16:32:16 +0800

    fix deprecate warning

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -24,13 +24,13 @@ module ApplicationHelper
   def tab_to(name, all_options = nil)
     url = all_options.is_a?(Array) ? all_options[0].merge({:only_path => false}) : "#"
 
-    current_url = url_for(:action => @current_action, :only_path => false)
+    current_url = url_for(:action => controller.action_name, :only_path => false)
     html_options = {}
 
     if all_options.is_a?(Array)
       all_options.each do |o|
         if url_for(o.merge({:only_path => false})) == current_url
-          html_options = {:class => "current"}
+          html_options = {:class => "active"}
           break
         end
       end  
